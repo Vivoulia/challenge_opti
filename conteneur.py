2717bd79775db71b4625d4b86d6e126e6a8221d4
Vivien D <viviendebuchy@yahoo.fr>
2019-06-20 11:07:13 +0200

    correction bug

--- conteneur.py
+++ conteneur.py
@@ -2,10 +2,10 @@
 import queue
 
 class Conteneur:
-    def __init__(self, x, y, name):
+    def __init__(self, x, y, conteneur_id):
         self.x = x
         self.y = y
-        self.name = name
+        self.conteneur_id = conteneur_id
         self.ajouter = False
         
 class GestionnaireConteneur:
@@ -15,11 +15,30 @@ class GestionnaireConteneur:
         self.L = L
         self.tab_pile = []
         self.tab_conteneur = []
+        self.tab_operation = []
     
     def addPile(self):
         Q = queue.LifoQueue()
         self.tab_pile.append(Q)
+        
+    def addConteneurPile(cont, id_pile):
+        #Ajoute un conteneur dans la pile d'id id_pile
+        pass
     
-    def createConteneur(self, x, y, name):
-        conteneur = Conteneur(x, y, name)
-        tab_conteneur.append(conteneur)
+    def createConteneur(self, x, y, conteneur_id):
+        conteneur = Conteneur(x, y, conteneur_id)
+        self.tab_conteneur.append(conteneur)
+        
+    def addOperation(self, operation):
+        self.tab_operation.append(operation)
+    
+    def estAccessible(self, conteneur_id):
+        for i in range(len(self.tab_pile)):
+            if self.tab_pile[i].empty() == False:
+                contenenur_sommet = self.tab_pile[i].get_nowait()
+                if (contenenur_sommet.conteneur_id == conteneur_id):
+                    return True
+        return False
+            
+            
+    
--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import conteneur
 
+#def first_fit_heuristic(gestionnaire):
+    #for operation in gestionaire.operation:
+        #conteneur_id = operation[0]
+        #conteneur_operation = operation[1]
+        #if conteneur_operation == "Ajout":
+            #for i in range(len(gestionnaire.tab_pile)):
+                #if (gestionnaire.tab_pile[i].empty()):
+                    #gestionnaire.tab_pile[i].put(tab_conteneur[conteneur_id])
+        #else:
+            #while():
+
 def main():
     print("c'est le main")
-    cont = conteneur.Conteneur(10,3,4)
-    print(cont.name)
+    gestionnaire = conteneur.GestionnaireConteneur(4,3,5)
+    gestionnaire.addPile()
+    gestionnaire.createConteneur(1,1,0)
+    gestionnaire.tab_pile[0].put(gestionnaire.tab_conteneur[0])
+    print(gestionnaire.estAccessible(0))
     
 if __name__ == "__main__":
     main()   
