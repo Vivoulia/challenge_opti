374a3a618ba2b8d221a52338eafb8ee63e2c7a38
Vivien D <viviendebuchy@yahoo.fr>
2019-06-20 10:30:27 +0200

    séparation fichier données

--- main.py
+++ main.py
@@ -1,32 +1,9 @@
+import conteneur
 
-import queue
-
-class Conteneur:
-    def __init__(self, x, y, name):
-        self.x = x
-        self.y = y
-        self.name = name
-        self.ajouter = False
-        
-class GestionnaireConteneur:
-    def __init__(self, N, L, H):
-        self.N = N
-        self.H = H
-        self.L = L
-        self.tab_pile = []
-        self.tab_conteneur = []
-    
-    def addPile(self):
-        Q = queue.LifoQueue()
-        self.tab_pile.append(Q)
-    
-    def createConteneur(self, x, y, name):
-        conteneur = Conteneur(x, y, name)
-        tab_conteneur.append(conteneur)
-    
 def main():
     print("c'est le main")
-    
+    cont = conteneur.Conteneur(10,3,4)
+    print(cont.name)
     
 if __name__ == "__main__":
     main()   
